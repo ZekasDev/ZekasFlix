34a28f2321a05d2648cfaf4a9de7fbab2f1da6e9
ZekasDev <dannndourado@gmail.com>
2023-04-23 19:21:39 -0300

    ZekasGram

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,76 @@
+import NextAuth from "next-auth/next";
+import Credentials from "next-auth/providers/credentials";
+import { compare } from "bcrypt";
+
+import GoogleProvider from "next-auth/providers/google";
+import FacebookProvider from "next-auth/providers/facebook";
+
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+
+import prismadb from "@/libs/prismadb";
+
+export default NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID || '' ,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || '',
+    }), 
+    FacebookProvider({
+      clientId: process.env.FACEBOOK_CLIENT_ID || '' ,
+      clientSecret: process.env.FACEBOOK_CLIENT_SECRET || '',
+    }), 
+
+    Credentials({
+      id: "credentials",
+      name: "Credentials",
+      credentials: {
+        email: {
+          label: "Email",
+          type: "text",
+        },
+        password: {
+          label: "Password",
+          type: "password",
+        },
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials?.password) {
+          throw new Error("E-mail e Senha são obrigatórios.");
+        }
+       
+        const user = await prismadb.user.findUnique({
+          where: {
+            email: credentials.email,
+          },
+        });
+
+        if (!user || !user.hashedPassword) {
+          throw new Error("E-mail não existe.");
+        }
+
+        const isCorrectPassword = await compare(
+          credentials.password,
+          user.hashedPassword
+        );
+
+        if (!isCorrectPassword) {
+          throw new Error("Senha errada. Digite novamente.");
+        }
+
+        return user;
+      },
+    }),
+  ],
+  pages: {
+    signIn: "/auth",
+  },
+  debug: process.env.NODE_ENV == "development",
+  adapter: PrismaAdapter(prismadb),
+  session: {
+    strategy: "jwt",
+  },
+  jwt: {
+    secret: process.env.NEXTAUTH_JWT_SECRET,
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+});
